386f1881bd40435171a90ffa1b054cfd7f8ff065
John Martin <john.itvn@gmail.com>
2025-03-28 21:54:36 +0000

    chore: add provide logging

--- apps/app-site/src/app/app.config.ts
+++ apps/app-site/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { provideLogging } from "@ecoma/nge-logging";
 import { appRoutes } from "./app.routes";
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideLogging(),
   ],
 };
--- apps/home-site/src/app/app.config.ts
+++ apps/home-site/src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import {
   provideClientHydration,
   withEventReplay,
-} from '@angular/platform-browser';
+} from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { provideLogging } from "@ecoma/nge-logging";
+import { appRoutes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideLogging(),
   ],
 };
