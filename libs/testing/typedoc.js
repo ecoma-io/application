1cbefca7cb645e86dc697ddac000eff4adeaaf96
John Martin <john.itvn@gmail.com>
2025-05-28 21:19:32 +0000

    feat(testing): introduce common testing utilities and containers
    Implement a library providing reusable testing utilities, including
    Docker containers for various services (MongoDB, PostgreSQL, NATS,
    RabbitMQ, Redis, Maildev, ToxiProxy) and supporting utilities like
    a Maildev client and a test logger.# Please enter the commit message for your changes. Lines starting

---
+++ libs/testing/typedoc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('typedoc').TypeDocOptions}
+ */
+module.exports = {
+  extends: [
+    "../../typedoc.base.js", // Điều chỉnh đường dẫn này nếu file preset ở vị trí khác
+  ],
+  entryPoints: ["./src/index.ts"],
+  out: "../../docs/api/testing",
+  publicPath: "/api/testing",
+  name: "@ecoma/testing",
+  entryFileName: "testing",
+};
