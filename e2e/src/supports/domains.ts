c387d53b398a5048d80f5b64456ee488c6a05bcc
John Martin <john.itvn@gmail.com>
2025-05-29 17:13:00 +0000

    feat(e2e): add Playwright configuration and SSR tests
    - Introduced Playwright configuration for end-to-end testing in e2e/playwright.config.ts.
    - Created ESLint configuration for Playwright in e2e/esline.config.mjs.
    - Added project.json to define e2e project settings and targets.
    - Implemented TypeScript configuration in e2e/tsconfig.json.
    - Developed SSR tests for multiple sites in e2e/src/specs/ssr.spec.ts.
    - Defined domain URLs in e2e/src/supports/domains.ts for test automation.

---
+++ e2e/src/supports/domains.ts
@@ -0,0 +1,5 @@
+export const BASE_DOMAIN = process.env["BASE_DOMAIN"];
+export const HOME_SITE_URL = `https://${BASE_DOMAIN}`;
+export const ACCOUNTS_SITE_URL = `https://accounts.${BASE_DOMAIN}`;
+export const ADMIN_SITE_URL = `https://admin.${BASE_DOMAIN}`;
+export const APP_SITE_URL = `https://app.${BASE_DOMAIN}`;
