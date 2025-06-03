410d5d43ab91b496bf8ba4492ef82fff74a52e68
John Martin <john.itvn@gmail.com>
2025-06-03 09:54:53 +0000

    chore: update project.json configurations to disable host check and set host to 0.0.0.0 for app and home sites

--- apps/accounts-site/src/app/features/dashboard/profile/profile.component.ts
+++ apps/accounts-site/src/app/features/dashboard/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
@@ -24,7 +24,11 @@ import { Domains } from '@ecoma/nge-domain';
                   <span class="label-text font-medium">First Name</span>
                 </label>
                 <div class="relative">
-                  <nge-svg-injector [path]="iconUrl('/duotone/user.svg')" class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"> </nge-svg-injector>
+                  <nge-svg-injector
+                    [path]="iconUrl('/duotone/user.svg')"
+                    class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"
+                  >
+                  </nge-svg-injector>
                   <input
                     type="text"
                     formControlName="firstName"
@@ -44,7 +48,11 @@ import { Domains } from '@ecoma/nge-domain';
                   <span class="label-text font-medium">Last Name</span>
                 </label>
                 <div class="relative">
-                  <nge-svg-injector [path]="iconUrl('/duotone/user.svg')" class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"> </nge-svg-injector>
+                  <nge-svg-injector
+                    [path]="iconUrl('/duotone/user.svg')"
+                    class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"
+                  >
+                  </nge-svg-injector>
                   <input
                     type="text"
                     formControlName="lastName"
@@ -58,15 +66,17 @@ import { Domains } from '@ecoma/nge-domain';
                 </label>
               </div>
 
-
-
               <!-- Phone -->
               <div class="form-control">
                 <label class="label" for="phoneInput">
                   <span class="label-text font-medium">Phone Number</span>
                 </label>
                 <div class="relative">
-                  <nge-svg-injector [path]="iconUrl('/duotone/phone.svg')" class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"> </nge-svg-injector>
+                  <nge-svg-injector
+                    [path]="iconUrl('/duotone/phone.svg')"
+                    class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"
+                  >
+                  </nge-svg-injector>
                   <input
                     type="tel"
                     formControlName="phone"
@@ -86,7 +96,11 @@ import { Domains } from '@ecoma/nge-domain';
                   <span class="label-text font-medium">Date of Birth</span>
                 </label>
                 <div class="relative">
-                  <nge-svg-injector [path]="iconUrl('/duotone/phone.svg')" class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"> </nge-svg-injector>
+                  <nge-svg-injector
+                    [path]="iconUrl('/duotone/phone.svg')"
+                    class="h-4 w-4 absolute left-3 top-1/2 -translate-y-1/2 text-base-content/50"
+                  >
+                  </nge-svg-injector>
                   <input type="date" formControlName="dateOfBirth" class="input input-bordered w-full pl-10" id="dateOfBirthInput" />
                 </div>
                 <label
@@ -98,7 +112,6 @@ import { Domains } from '@ecoma/nge-domain';
                 </label>
               </div>
             </div>
-
 
             <!-- Action Buttons -->
             <div class="flex justify-end gap-4">
@@ -114,16 +127,12 @@ import { Domains } from '@ecoma/nge-domain';
     </div>
   `,
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   profileForm: FormGroup;
   isLoading = false;
   iconsBaseUrl: string;
 
-  constructor(
-    private fb: FormBuilder,
-    private title: Title,
-    private domain: Domains
-  ) {
+  constructor(private fb: FormBuilder, private title: Title, private domain: Domains) {
     this.title.setTitle('Profile');
     this.iconsBaseUrl = this.domain.getIconsBaseUrl();
     this.profileForm = this.fb.group({
@@ -136,18 +145,6 @@ export class ProfileComponent implements OnInit {
 
   iconUrl(path: string): string {
     return this.iconsBaseUrl + path;
-  }
-
-  ngOnInit() {
-    // Load user data
-    // const userData = localStorage.getItem('user_data');
-    // if (userData) {
-    //   const user = JSON.parse(userData);
-    //   this.profileForm.patchValue({
-    //     fullName: user.name,
-    //     email: user.email,
-    //   });
-    // }
   }
 
   ageValidator(control: any) {
@@ -176,7 +173,6 @@ export class ProfileComponent implements OnInit {
 
   resetForm() {
     this.profileForm.reset();
-    this.ngOnInit();
   }
 
   onSubmit() {
