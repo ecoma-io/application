549295f0df6d379f422df267eb1c4fe4d529e8c9
John Martin <john.itvn@gmail.com>
2025-06-04 01:24:21 +0000

    refactor: simplify global setup and teardown functions by removing console logs

--- apps/iam-service-integration/src/support/global-teardown.ts
+++ apps/iam-service-integration/src/support/global-teardown.ts
@@ -1,10 +1,3 @@
 /* eslint-disable */
 
-module.exports = async function () {
-  console.log(
-    "\nTearing down test environment for ALM Ingestion E2E tests...\n"
-  );
-
-  // Không dọn dẹp container ở đây, chúng ta sẽ thực hiện trong file spec
-  console.log("\nTest environment teardown completed successfully!\n");
-};
+module.exports = async function () {};
